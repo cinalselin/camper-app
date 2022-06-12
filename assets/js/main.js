5a1c7f70d43ab49d424b6e3be6da5e1156fcc092
“cinalselin” <hello@selincinal.de>
2022-06-12 15:02:56 +0200

    fixed navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 function change(event) {
     event.preventDefault();
-    document.getElementById("navbar").classList.toggle("navFlow");
+    document.querySelector("nav").classList.toggle("iconShow");
     document.getElementById("click").classList.toggle("active2");
 }
 
-
+function changeX(event) {
+    event.preventDefault();
+    document.querySelector("nav").classList.remove("iconShow");
+    document.getElementById("click").classList.remove("active2");
+}
